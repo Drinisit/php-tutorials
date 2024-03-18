1e4b209d457c7c02399983b48e631310e362a87b
Drin <aldrinisit3196@gmail.com>
2024-03-19 05:52:05 +0800

    Add: if_elseif_else

---
+++ if_else.php
@@ -0,0 +1,23 @@
+<?php
+
+    // $num = 10;
+    // $num2 = 5;
+    // $num3 = 5.2;
+
+    // if($num > $num2){
+    //     echo "True";
+    // }else{
+    //     echo "False";
+    // }
+
+    $day = 'Tuesday';
+
+    if($day == 'Friday'){
+        echo 'Its weekdays';
+    }elseif($day == 'Sunday'){
+        echo 'Its weekend';
+    }else{
+        echo 'its not weekend as well not Friday';
+    }
+
+?>
---
+++ operators.php
@@ -0,0 +1,28 @@
+<?php
+
+    $num = 10;
+    $num2 = 7;
+    $num3 = 3.4;
+
+    echo $num + $num3; 
+    echo "<br/>";
+    echo $num - $num2;
+    echo "<br/>";
+    echo $num * $num3;
+    echo "<br/>";
+    echo $num / $num2;
+    echo "<br/>";
+    echo $num % $num2;
+    echo "<br/>";
+    echo $num ** $num2;
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo $num = $num3;
+    echo "<br/>";
+    echo $num += $num3;
+    echo "<br/>";
+    echo $num -= $num3;
+
+
+?>
