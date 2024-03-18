b3ce759a191add87f6aa395a16bfffae132b38ae
Drin <aldrinisit3196@gmail.com>
2024-03-18 20:50:23 +0800

    Add: data types

---
+++ data_types.php
@@ -0,0 +1,9 @@
+<?php
+    $text = "Hello";
+    $num = 27;
+    $float = 5.6;
+    $boolean = true;
+    $array =  array("Aldrin", "Ade", 27, true);
+
+   var_dump($array);
+?>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php 
 
-    echo 'Hello ADE!'
+$var1 = "Hello";
+$var_num = 27;
+$first_name = "Aldrin Jay";
+
+echo $var_num;
+echo "<br/>";
+echo $var1;
+
+echo $first_name;
+
 ?>
