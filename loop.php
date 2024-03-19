f575d7bc86494f18b983fbc203ea2d547fd53bd3
Drin <aldrinisit3196@gmail.com>
2024-03-19 20:34:06 +0800

    Add: foreach loop

--- loop.php
+++ loop.php
@@ -1,6 +1,10 @@
 <?php
 
-$x = 9;
+$text = "Hello";
+    $num = 27;
+    $float = 5.6;
+    $boolean = true;
+    $data_types =  array($text, $num, $float, $boolean);
 
 // while($x <= 10){
 //     echo "Ang bilang nato ay pang ".$x."<br/>";
@@ -12,9 +16,12 @@ $x = 9;
 //     $x++;
 // }while($x <= 8);
 
-for($i = 1; $i <= 5; $i++){
-    echo "Eto ay pang ". $i. "<br/>";
-}
+// for($i = 1; $i <= 5; $i++){
+//     echo "Eto ay pang ". $i. "<br/>";
+// }
 
+foreach($data_types as $values){
+    echo $values."<br/>";
+}
 
 ?>
