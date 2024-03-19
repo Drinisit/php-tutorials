6fee7a638062714ba93cd21bf733091ffc7175eb
Drin <aldrinisit3196@gmail.com>
2024-03-19 20:26:51 +0800

    Add: for loop

--- loop.php
+++ loop.php
@@ -1,10 +1,19 @@
 <?php
 
-$x = 3;
+$x = 9;
 
-while($x <= 10){
-    echo "Ang bilang nato ay pang ".$x."<br/>";
-    $x++;
+// while($x <= 10){
+//     echo "Ang bilang nato ay pang ".$x."<br/>";
+//     $x++;
+// }
+
+// do{
+//     echo "Ang bilang na to ay " .$x. "<br/>";
+//     $x++;
+// }while($x <= 8);
+
+for($i = 1; $i <= 5; $i++){
+    echo "Eto ay pang ". $i. "<br/>";
 }
 
 
