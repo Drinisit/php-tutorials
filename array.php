349df5dcc2aa751a5490d1cfc5400c9e7eed8cb7
Drin <aldrinisit3196@gmail.com>
2024-03-20 20:34:42 +0800

    Add: array with loop

--- array.php
+++ array.php
@@ -2,12 +2,28 @@
 
    $brands = array("honda", "toyota","suzuki","ford","nissan");
 
-   $total = count($brands);
+ 
+   // $total = count($brands);
 
- echo "The total of cars is ". $total;
+   // echo "Number of cars ".$total;
 
- for($x = 0; $x < $total; $x++){
-    echo $brands[$x]. "<br/>";
- }
+   // for($total < 0; $total < count($brands); $total++){
+   //       echo "The car brand is ".$brands[$total]."<br/>";
+   // }
 
+
+   // do{
+   //    echo "The car brand is ".$brands[$total]."<br/>";
+   //    $total++;
+   // }while($total < count($brands));
+
+   // while($total < count($brands)){
+   //    echo "The car brand is ".$brands[$total]. "<br/>";
+   //    $total++;
+   // }
+
+   foreach($brands as $cars){
+      echo "The brands of car is ".$cars. "<br/>";
+   }
+   
 ?>
