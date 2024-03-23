5f2ecb12df83785bedf7f68c4b9396fcf726c97a
Drin <aldrinisit3196@gmail.com>
2024-03-23 21:17:14 +0800

    Update: add html on php file

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-$var1 = "Hello";
-$var_num = 27;
-$first_name = "Aldrin Jay";
+    <p>This is a <?php echo "Pangungusap"; ?> paragraph</p>
+    <?php echo "This is also a paragraph"; ?>
 
-echo "Name: $first_name";
+    <br/>
 
-?>
+
+    <?php
+
+        $firstName = "Ade";
+        $age = 27;
+
+       var_dump($age);
+
+    ?>  
+
+</body>
+</html>
