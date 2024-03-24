155087e5458bcaf11ffe699cb2b57c76372441ae
Drin <aldrinisit3196@gmail.com>
2024-03-24 11:55:32 +0800

    Add: form

--- index.php
+++ index.php
@@ -7,26 +7,23 @@
 </head>
 <body>
 
-    <p>This is a <?php echo "Pangungusap"; ?> paragraph</p>
-    <?php echo "This is also a paragraph"; ?>
-
-    <br/>
-    <br/>
-
-
-    <?php
-
-    $_SESSION["username"] = "Ade";
-    echo $_SESSION["username"];
-
-   
-
-    //     $firstName = "Ade";
-    //     $age = 27;
-
-    // ?>  
-
-    <!-- // <p> Hi my name is <?php  echo $firstName; ?></p> -->
+   <main>
+   <form action="includes/formhandler.php" method="post">
+        <label for="firstname">First name:</label>
+        <input type="text" id="firstname" name="firstname" placeholder="First Name">
+        <label for="lastname">Last name:</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Last Name">
+        
+        <label for="car" >Favorite Car</label>
+        <select id="car" name="car">
+            <option value="bmw">bmw</option>
+            <option value="honda">honda</option>
+            <option value="toyota">toyota</option>
+            <option value="subaru">subaru</option>
+        </select>
+        <button type="submit">Submit</button>
+    </form>
+   </main>
 
 </body>
 </html>
