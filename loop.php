ce8ed9457c38d87b8dd279a3dcdbd30ff20c2c30
Drin <aldrinisit3196@gmail.com>
2024-03-25 21:07:11 +0800

    Update: loops

--- loop.php
+++ loop.php
@@ -1,10 +1,67 @@
 <?php
-    $x = 11;
+    $x = 0;
+    $i = 0;
     $text = "Hello";
     $num = 27;
     $float = 5.6;
     $boolean = true;
     $data_types =  array($text, $num, $float, $boolean);
+
+
+    foreach($data_types as $data){
+        echo $data ."<br>";
+    }
+
+
+    // do{
+    //     echo "This will display the number". $i."<br/>";
+    //     $i++;
+    // }while($i <= 10);
+
+//     do{
+//     echo "Ang bilang na to ay " .$x. "<br/>";
+//     $x++;
+// }while($x <= 8);
+
+    // while($x <= 10){
+    //     echo "This execute the number $x ". "<br/>";
+    //     $x++;
+    // }
+
+
+    // for($i = 1; $i <= $x; $i++){
+    //     echo "This is number ". $i. "<br/>";
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // while($x <= 10){
 //     echo "Ang bilang nato ay pang ".$x."<br/>";
