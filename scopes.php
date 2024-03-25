638d0941eb146104c8355644de2e23ba75fc06fa
Drin <aldrinisit3196@gmail.com>
2024-03-25 18:39:11 +0800

    Add: scopes

---
+++ scopes.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    class myClass{
+        public $classVar = "Hello World";
+
+        public function myMethod(){
+            echo $this->classVar;
+        }
+    }
+
+
+
+
+    // $text = "Ade";
+
+    // function func(){
+    //    static $staticVar = 0;
+
+    //     $staticVar++;
+
+    //     return $staticVar;
+    // }
+
+    // echo func();
+    // echo func();
+
+
+// function greet(){
+
+//     $hi = "Hello ade";
+
+//     return $hi;
+// }
+
+// echo greet();
+
+
+    ?>
+
+</body>
+</html>
