6ff958722419d353fe729aaadadc777b707ae76b
Drin <aldrinisit3196@gmail.com>
2024-03-25 18:46:22 +0800

    Update: constant

--- constant.php
+++ constant.php
@@ -1,9 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
- $car = "Honda";
+        define("PI", 3.14);
+        define("NAME", "ADE");
 
- define("Ade", "Isit");
 
- echo Ade;
+        function test(){
+            echo PI;
+        }
 
-?>
+        test();
+
+
+
+
+
+    ?>
+</body>
+</html>
