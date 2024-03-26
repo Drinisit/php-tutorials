40c62f1afaea9e4f2e6482d9d1bb8960771f2dbb
Drin <aldrinisit3196@gmail.com>
2024-03-26 20:59:42 +0800

    Update: exercise 3

--- basicalgo.php
+++ basicalgo.php
@@ -10,16 +10,35 @@
 // 21
 // 0
 
-    function diff($n){
-        $x = 51;
-        if($n > $x ){
-            return ($n - $x)*3;
-        }else{
-            return $x - $n ;
-        }
+    // function diff($n){
+    //     $x = 51;
+    //     if($n > $x ){
+    //         return ($n - $x)*3;
+    //     }else{
+    //         return $x - $n ;
+    //     }
 
      
-    };
+    // };
 
-    echo diff(51);
+    //echo diff(51);
+
+//     3. Write a PHP program to check two given integers, and return true if one of them is 30 or if their sum is 30.
+// Sample Input:
+// 30, 0
+// 25, 5
+// 20, 30
+// 20, 25
+// Sample Output:
+// bool(true)
+// bool(true)
+// bool(true)
+// bool(false)
+
+    function sum($x, $y){
+       
+        return ($x == 30) || ($y == 30) ||($x + $y == 30);
+    }
+
+    var_dump(sum(30,0));
    
