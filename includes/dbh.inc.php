7e3a6052039fac238a7289ca83d8fb1a8227e364
Drin <aldrinisit3196@gmail.com>
2024-03-28 07:14:49 +0800

    Add: user update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,8 +5,11 @@
     $dbpassword = "";
 
     try {
+        //this line is to connect to database
         $pdo = new PDO( $dsn, $dbusername, $dbpassword);
+        //to see if the is an error
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
+        //display message for an error
         echo "Connection failed: ". $e->getMessage();
     }
--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 
     <main>
 
+    <h3>Update account</h3>
+    <form action="includes/userupdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="password" name="pwd" placeholder="Password"><br>
+        <input type="text" name="email" placeholder="Email"><br>
+        <button>Update User</button>
+    </form>
+    <br>
+    <br>
+    <h3>Signup account</h3>
     <form action="includes/formhandler.inc.php" method="post">
         <input type="text" name="username" placeholder="Username"><br>
         <input type="password" name="pwd" placeholder="Password"><br>
