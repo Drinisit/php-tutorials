aa2d11976ff69a9fecee098d359e37cd3daea9da
Drin <aldrinisit3196@gmail.com>
2024-03-31 22:37:53 +0800

    Add: color sort

--- test.php
+++ test.php
@@ -3,5 +3,19 @@
 
         echo "   The memory of that scene for me is like a frame of film forever frozen at that moment: the  $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon";
 
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+    $color2 = array('white', 'green', 'red');  
+    
+      
+
+        sort($color2);
+
+        foreach($color2 as $y){
+            echo "<li> $y";
+        }
+
 ?>
 
