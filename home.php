eae468a11311c2161bfac3b587f3480084286f61
Drin <aldrinisit3196@gmail.com>
2024-03-31 21:03:05 +0800

    Add: email validation

--- home.php
+++ home.php
@@ -1,8 +1,15 @@
 <?php
 
- if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["firstName"]) && isset($_POST["lastName"])){
+ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["firstName"]) && isset($_POST["lastName"]) && isset($_POST["email"])){
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "<h3> Hello $firstName $lastName Your $email is Valid<h3/>";
+    }else{
+        echo "<h3> Hello $firstName $lastName Your $email is Invalid<h3/>";
+    }
 
     echo "<h3> Hello $firstName $lastName <h3/>";
  }
@@ -21,6 +28,7 @@
     <form action="home.php" method="post">
         <input type="text" name="firstName" placeholder="First Name"><br>
         <input type="text" name="lastName" placeholder="Last Name"><br>
+        <input type="text" name="email" placeholder="Email"><br>
         <button type="submit">Submit</button>
     </form>
     
