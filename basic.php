5f52a24b9458d84bc8e0cfd3733853d3090b75ba
Drin <aldrinisit3196@gmail.com>
2024-04-01 10:28:22 +0800

    Add: dot increment

--- basic.php
+++ basic.php
@@ -10,13 +10,32 @@
 //     }
 // }
 
-function addition($num1, $num2){
-    if($num1 <= 30 && $num2 <=30){
-        $add = $num1 + $num2;
-        return $add;
-    }else{
-        echo "The number is too high cannot be add";
-    }
-}
+// function addition($num1, $num2){
+//     if($num1 <= 30 && $num2 <=30){
+//         $add = $num1 + $num2;
+//         return $add;
+//     }else{
+//         echo "The number is too high cannot be add";
+//     }
+// }
 
-echo addition(4,31);
+// echo addition(31,0);
+
+// $sum = 0;
+ 
+// for($i = 0; $i<=30; $i++){
+//     $sum += $i;
+// }
+
+// echo "The sum of the numbers 0 to 30 is $sum" . "\n";
+
+for($i = 1; $i <= 5; $i++){
+    for($a =1; $a <= $i; $a++){
+        echo "*";
+
+        if($a < $i){
+            echo " ";
+        }
+    }
+    echo "<br>";
+}
