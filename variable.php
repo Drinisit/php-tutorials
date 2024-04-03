ede1e4d56c96ab04cc329588f6360f0e30a2f516
Drin <aldrinisit3196@gmail.com>
2024-04-03 21:05:51 +0800

    Add: var

---
+++ variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    $num1 = 1;
+    $num2 = 3;
+
+    $add = $num1 + $num2;
+
+    echo $add;
+
+    ?>
+
+</body>
+</html>
