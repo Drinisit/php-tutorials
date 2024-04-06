b2314aad740fdba7b201b457e9d7391d62b99443
Drin <aldrinisit3196@gmail.com>
2024-04-06 23:01:33 +0800

    Add: ternary operators

--- operator.php
+++ operator.php
@@ -10,5 +10,21 @@ var_dump($x !== $y);
 
 // echo $z;
 
+echo "<br>";
+
+$a = "Hello";
+$b = strpos($a, 'H');
+
+if($b === false){
+    echo "H is not found";
+}else{
+    echo "H is found " . $b;
+}
+
+echo "<br> Ternary Operator";
+
+$result = $y === false ? "H is not found" : "H is found" . $y;
+echo "<br>" . $result;
+
 
 
