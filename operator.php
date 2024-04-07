ae8e13bc04dfa6e718e2ee1d3da6534f01b9e5e7
Drin <aldrinisit3196@gmail.com>
2024-04-07 22:54:09 +0800

    Add: array operators

--- operator.php
+++ operator.php
@@ -22,9 +22,19 @@ if($b === false){
 }
 
 echo "<br> Ternary Operator";
-
 $result = $y === false ? "H is not found" : "H is found" . $y;
 echo "<br>" . $result;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$brand = ['toyota' => 1, 'honda' => 2,'nissan'=> 3, 'BMV' => 4];
+$carType = ['van' => 5,'pickup'=> 6,'sedan'=> 7];
+
+$cars = $brand + $carType;
+print_r($cars);
+
+
 
 
 
