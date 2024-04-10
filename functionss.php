443e346fe3962f5d7347519483629b377633b2f5
Drin <aldrinisit3196@gmail.com>
2024-04-10 21:50:40 +0800

    Add: function with splat operators

--- functionss.php
+++ functionss.php
@@ -2,8 +2,21 @@
 
 declare(strict_types=1);
 
-function foo(int|float $x, int|float $y){
-    return $x + $y;
+// function foo(int|float $x, int|float $y){
+//     return $x + $y;
+// }
+
+
+function sum(...$numbers): int | float{
+    $sum = 0;
+
+    foreach($numbers as $number){
+        $sum += $number;
+    }
+
+    return $sum;
 }
 
-echo foo(3.5,5);
+echo sum( 50, 100,67);
+
+// echo foo(3.5,5);
