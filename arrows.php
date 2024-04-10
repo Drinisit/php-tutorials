a46f0aa13ac480827a8601f04be24bb389c61827
Drin <aldrinisit3196@gmail.com>
2024-04-11 05:45:33 +0800

    Add: arrow function

--- arrows.php
+++ arrows.php
@@ -10,4 +10,17 @@ $sum = function (int|float ...$numbers): int|float{
     return array_sum($numbers);
 };
 
-echo $sum(1,2,4,5);
+echo $sum(1,2,4,5) . "<br>";
+
+
+
+//arrow function
+$array = [1,2,3,4,5];
+
+$y = 5;
+
+$array2 = array_map(fn($number)=> $number * $number * $y, $array);
+
+echo "<pre>";
+print_r($array2);
+echo "</pre>";
