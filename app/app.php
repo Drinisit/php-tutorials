e2343481591494bc32df9d34728815ffa936afc8
Drin <aldrinisit3196@gmail.com>
2024-04-13 22:10:09 +0800

    Add: remove
    remove dollar sign and comma from amount

--- app/app.php
+++ app/app.php
@@ -26,9 +26,23 @@ function getTransactions(string $fileName): array{
 
     $transactions = [];
 
-    while(($transaction = fgetcsv($file)) != false){
-        $transactions[] = $transaction;
+    while(($transaction = fgetcsv($file)) !== false){
+        $transactions[] = extractTransaction($transaction);
     }
 
     return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array{
+
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return [
+        'date'=> $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount' => $amount,
+    ];
 }
