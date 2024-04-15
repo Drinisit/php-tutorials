e58103f039238a924c24a9996f7384c3e70a7e8f
Drin <aldrinisit3196@gmail.com>
2024-04-15 23:01:30 +0800

    Add: method addTax

--- intermediate/index.php
+++ intermediate/index.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 require_once './Transaction.php';
 
-$transaction = new Transaction(15, 'This is a transaction');
+$transaction = new Transaction(100, 'This is a transaction');
+$transaction->addTax(50);
 
 
 var_dump($transaction->amount);
