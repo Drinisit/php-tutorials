1a12824f2bc5610f4e9dbc9c2a4ca2ebbd30da8b
Drin <aldrinisit3196@gmail.com>
2024-04-16 21:08:04 +0800

    Add: separation amount

--- intermediate/index.php
+++ intermediate/index.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 require_once './Transaction.php';
 
-$amount =( new Transaction(100, 'This is a transaction'))
+$transaction1 =( new Transaction(100, 'This is a transaction 1'))
     ->addTax(50)
-    ->applyDiscount(10)
-    ->getAmount();
+    ->applyDiscount(10);
 
-var_dump($amount);
+
+$transaction2 =( new Transaction(200, 'This is a transaction 2'))
+    ->addTax(80)
+    ->applyDiscount(20);
+
+
+var_dump($transaction1->getAmount(), $transaction2->getAmount());
