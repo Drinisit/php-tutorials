38c94b32194c5e8b63cc6300f30e9949ff29cdf2
Drin <aldrinisit3196@gmail.com>
2024-04-17 22:22:46 +0800

    Add: null safe on customer

--- intermediate2/Transaction.php
+++ intermediate2/Transaction.php
@@ -4,11 +4,12 @@
 
  class Transaction2{
 
+    public ?Customer $customer = null;
+
     public function __construct(
        private float $amount, 
        private string $description){
             
-       echo $this->amount;
 
     }
         
--- intermediate2/index.php
+++ intermediate2/index.php
@@ -1,9 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
-// require_once './PaymentProfile.php';
-// require_once './Customer.php';
+require_once './PaymentProfile.php';
+require_once './Customer.php';
 require_once './Transaction.php';
 
 $transaction = new Transaction2(5, 'Test');
+
+echo $transaction->customer?->paymentProfile->id;
