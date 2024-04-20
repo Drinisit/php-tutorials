a4380da8bba151dc2b0635b4dd819d7605139ee0
Drin <aldrinisit3196@gmail.com>
2024-04-20 21:32:38 +0800

    Add: dateTIme

--- intermediate3/PaymentGateway/Paddle/Transaction.php
+++ intermediate3/PaymentGateway/Paddle/Transaction.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace PaymentGateway\Paddle;
 
+use DateTime;
+
 class Transaction{
     
     public function __construct(){
-        var_dump(new CustomerProfile());
+        var_dump(new DateTime());
     }
 }
