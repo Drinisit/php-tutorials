89285969af13b290961a182752bd156f1370f34a
Drin <aldrinisit3196@gmail.com>
2024-04-21 23:03:33 +0800

    Add: notification

--- intermediate3/PaymentGateway/Paddle/Transaction.php
+++ intermediate3/PaymentGateway/Paddle/Transaction.php
@@ -8,6 +8,6 @@ namespace PaymentGateway\Paddle;
 class Transaction{
     
     public function __construct(){
-        var_dump(new DateTime());
+        var_dump(new Notification\Email());
     }
 }
